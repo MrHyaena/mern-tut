5e25b12c59b7c830b079d57887f8bc4c12e0ad4a
MrHyaena <dolezalmartin131@gmail.com>
2024-12-03 16:10:30 +0100

    Created backend - MVC model

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workoutsRouter");
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  next();
+});
+
+// router handler
+app.use("/api/workouts", workoutRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server up on port 4000 and connected to database");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// listen for requests
